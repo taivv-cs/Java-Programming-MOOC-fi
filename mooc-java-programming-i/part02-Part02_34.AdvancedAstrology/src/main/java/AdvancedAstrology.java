b6d7ff48d31ab9909db4e5efebe19fb1859c946f
taivv18 <taivv18@gmail.com>
2026-04-19 01:58:03 +0700

    exercise(part02): complete AdvancedAstrology

---
+++ mooc-java-programming-i/part02-Part02_34.AdvancedAstrology/src/main/java/AdvancedAstrology.java
@@ -0,0 +1,46 @@
+
+public class AdvancedAstrology {
+
+    public static void printStars(int number) {
+        for (int i = 0;i < number; ++i) {
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+
+    public static void printSpaces(int number) {
+        for (int i = 0;i < number; ++i) {
+            System.out.print(" ");
+        }
+    }
+
+    public static void printTriangle(int size) {
+
+        for (int i = 1; i <= size; ++i) {
+            printSpaces(size - i);
+            printStars(i);
+        }
+    }
+
+    public static void christmasTree(int height) {
+        // part 3 of the exercise
+        int width = height * 2 - 1;
+        for (int i = 1; i <= height; ++i) {
+            printSpaces(height - i);
+            printStars(2 * i - 1);
+        }
+
+        for (int i = 1; i <= 2; ++i) {
+            printSpaces(height - 2);
+            printStars(3);
+        }
+    }
+
+    public static void main(String[] args) {
+        // The tests are not checking the main, so you can modify it freely.
+
+        printTriangle(0);
+        christmasTree(4);
+        christmasTree(10);
+    }
+}
