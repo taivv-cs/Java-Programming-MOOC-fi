a30f938058e33868d9186eb21e9af9f89c50eec1
taivv18 <taivv18@gmail.com>
2026-04-19 02:00:52 +0700

    exercise(part05): complete FitByte

---
+++ mooc-java-programming-i/part05-Part05_04.FitByte/src/main/java/Fitbyte.java
@@ -0,0 +1,16 @@
+public class Fitbyte {
+  private int age;
+  private int restingHeartRate;
+
+  public Fitbyte(int age, int restingHeartRate) {
+    this.age = age;
+    this.restingHeartRate = restingHeartRate;
+  }
+
+  // Đề bài cho 2 tham số, tuổi và nhịp tim nghỉ ngơi 
+  // Cần tính nhịp tim tối đa
+  public double targetHeartRate(double percentageOfMaximum) {
+    double maximumHeartRate = 206.3 - (0.711 * this.age);
+    return (maximumHeartRate - this.restingHeartRate) * percentageOfMaximum + this.restingHeartRate;
+ }
+}
