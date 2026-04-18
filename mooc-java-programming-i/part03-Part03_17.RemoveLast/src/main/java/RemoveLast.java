cc3ffaa5067f147e69f96eceb1d5f4121b0e9f8f
taivv18 <taivv18@gmail.com>
2026-04-19 01:58:45 +0700

    exercise(part03): complete RemoveLast

---
+++ mooc-java-programming-i/part03-Part03_17.RemoveLast/src/main/java/RemoveLast.java
@@ -0,0 +1,17 @@
+
+import java.util.ArrayList;
+
+public class RemoveLast {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void removeLast(ArrayList<String> strings) {
+        if (strings.isEmpty()) {
+            return;
+        }
+
+        strings.remove(strings.size() - 1);
+    }
+}
