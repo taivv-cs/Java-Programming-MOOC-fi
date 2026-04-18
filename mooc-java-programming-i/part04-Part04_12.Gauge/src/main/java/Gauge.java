b9c17ad18b86acb95eef4c58e5f24667e96b5608
taivv18 <taivv18@gmail.com>
2026-04-19 01:59:53 +0700

    exercise(part04): complete Gauge

---
+++ mooc-java-programming-i/part04-Part04_12.Gauge/src/main/java/Gauge.java
@@ -0,0 +1,27 @@
+public class Gauge {
+    private int value;
+
+    public Gauge() {
+        this.value = 0;
+    }
+
+    public void increase() {
+        if (value < 5) {
+            ++value;
+        }
+    }
+
+    public void decrease() {
+        if (value > 0) {
+            --value;
+        }
+    }
+
+    public int value() {
+        return this.value;
+    }
+
+    public boolean full() {
+        return this.value == 5;
+    }
+}
