ed5193aeed6caea21f0e25c6efd107a4aee464b7
taivv18 <taivv18@gmail.com>
2026-04-19 01:58:25 +0700

    exercise(part03): complete GreatestInList

---
+++ mooc-java-programming-i/part03-Part03_09.GreatestInList/src/main/java/GreatestInList.java
@@ -0,0 +1,32 @@
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class GreatestInList {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayList<Integer> list = new ArrayList<>();
+        while (true) {
+            int input = Integer.valueOf(scanner.nextLine());
+            if (input == -1) {
+                break;
+            }
+
+            list.add(input);
+        }
+
+        System.out.println("");
+
+        // implement finding the greatest number in the list here
+        int biggestNumber = list.get(0);
+        for (int i = 1; i < list.size(); ++i) {
+            if (biggestNumber <= list.get(i)) {
+                biggestNumber = list.get(i);
+            }
+        }
+
+        System.out.println("The greatest number: " + biggestNumber);
+    }
+}
