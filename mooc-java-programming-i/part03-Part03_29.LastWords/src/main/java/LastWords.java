9210cec5bcca61c2564186480e6eafcf2d14e07d
taivv18 <taivv18@gmail.com>
2026-04-19 01:59:14 +0700

    exercise(part03): complete LastWords

---
+++ mooc-java-programming-i/part03-Part03_29.LastWords/src/main/java/LastWords.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class LastWords {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String str = scanner.nextLine();
+            if (str.isEmpty()) {
+                break;
+            }
+
+            String[] arrayString = str.split(" ");
+            System.out.println(arrayString[arrayString.length - 1]);
+        }
+
+    }
+}
