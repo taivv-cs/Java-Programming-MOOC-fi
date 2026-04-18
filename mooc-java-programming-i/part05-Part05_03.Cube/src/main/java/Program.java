043b3817468fb952abc94078a54e17e1d86f5c71
taivv18 <taivv18@gmail.com>
2026-04-19 02:00:50 +0700

    exercise(part05): complete Cube

---
+++ mooc-java-programming-i/part05-Part05_03.Cube/src/main/java/Program.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class Program {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Cube oSheaJackson = new Cube(7);
+        System.out.println(oSheaJackson.volume());
+        System.out.println(oSheaJackson);
+
+        System.out.println();
+    }
+}
