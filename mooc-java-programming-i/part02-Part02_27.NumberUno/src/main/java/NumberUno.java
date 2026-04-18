f11569414d48e099d33e96e23323e0329286c664
taivv18 <taivv18@gmail.com>
2026-04-19 01:57:46 +0700

    exercise(part02): complete NumberUno

---
+++ mooc-java-programming-i/part02-Part02_27.NumberUno/src/main/java/NumberUno.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+
+public class NumberUno {
+
+    public static void main(String[] args) {
+        System.out.println(numberUno());
+    }
+
+    public static int numberUno() {
+        return 1;
+    }
+}
