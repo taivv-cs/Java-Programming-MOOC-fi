181fab5ab20005339ef01356cd3db86cc624f476
taivv18 <taivv18@gmail.com>
2026-05-12 00:28:41 +0700

    exercise(part04): completed YourFirstAccount

---
+++ mooc-java-programming-i/part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
@@ -0,0 +1,12 @@
+
+public class YourFirstAccount {
+
+    public static void main(String[] args) {
+        // Do not touch the code in Account.java
+        // Write your program here
+        Account taivv18 = new Account("Tai Vu Viet account", 100.00);
+
+        taivv18.deposit(20);
+        System.out.println(taivv18);
+    }
+}
