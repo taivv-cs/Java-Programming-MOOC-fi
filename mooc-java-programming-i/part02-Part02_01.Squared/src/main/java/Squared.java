64db9953af62d85b497c95082aee87ab8084ff0b
taivv18 <taivv18@gmail.com>
2026-05-12 18:26:09 +0700

    chore: format indentaion and submit

--- mooc-java-programming-i/part02-Part02_01.Squared/src/main/java/Squared.java
+++ mooc-java-programming-i/part02-Part02_01.Squared/src/main/java/Squared.java
@@ -2,10 +2,10 @@ import java.util.Scanner;
 
 public class Squared {
 
-  public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-    int num = Integer.valueOf(scanner.nextLine());
-    System.out.println(num * num);
-  }
+        int num = Integer.valueOf(scanner.nextLine());
+        System.out.println(num * num);
+    }
 }
