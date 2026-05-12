1be27675c4182f99d63b1b14d062f37a9d8a35ed
taivv18 <taivv18@gmail.com>
2026-05-12 18:35:09 +0700

    exercise(part06): completed MesaginService

--- mooc-java-programming-i/part06-Part06_03.MessagingService/src/main/java/MessagingService.java
+++ mooc-java-programming-i/part06-Part06_03.MessagingService/src/main/java/MessagingService.java
@@ -2,19 +2,19 @@ import java.util.ArrayList;
 
 public class MessagingService {
 
-  private ArrayList<Message> messages;
+    private ArrayList<Message> messages;
 
-  public MessagingService() {
-    this.messages = new ArrayList<>();
-  }
-
-  public void add(Message message) {
-    if (message.getContent().length() <= 280) {
-      messages.add(message);
+    public MessagingService() {
+        this.messages = new ArrayList<>();
     }
-  }
 
-  public ArrayList<Message> getMessages() {
-    return this.messages;
-  }
+    public void add(Message message) {
+        if (message.getContent().length() <= 280) {
+            messages.add(message);
+        }
+    }
+
+    public ArrayList<Message> getMessages() {
+        return this.messages;
+    }
 }
